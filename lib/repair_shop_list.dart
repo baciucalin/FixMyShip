3fb8177b450f928e3fba779e5906979140bfd2fd
Calin Baciu <calin.baciu@wirtek.com>
2020-04-11 13:51:44 +0300

    Add mock data and search feature for repair shops list

--- lib/repair_shop_list.dart
+++ lib/repair_shop_list.dart
@@ -1,16 +1,32 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:shipmyfix/repair_shop_card.dart';
 import 'package:shipmyfix/repair_shop_dto.dart';
 
-class RepairShopList extends StatelessWidget {
-  List<RepairShopDTO> repairShops; // RepairShop
+class RepairShopList extends StatefulWidget {
+  @override
+  RepairShopListState createState() => new RepairShopListState();
+}
+
+class RepairShopListState extends State<RepairShopList> {
+  List<RepairShopDTO> repairShops;
+  List<RepairShopDTO> repairShopsSearchResults;
+  TextEditingController _textEditingController = new TextEditingController();
+
   Function onRepairShopClicked;
 
-//  RepairShopList({this.repairShops, this.onRepairShopClicked});
-  RepairShopList();
+  RepairShopListState({this.repairShops, this.onRepairShopClicked});
+
+  @override
+  void initState() {
+    super.initState();
+    repairShops = [];
+    repairShopsSearchResults = [];
+    loadAsset(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +36,28 @@ class RepairShopList extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Container(
-            height: 100,
-            child: TextField(
-              autofocus: false,
-              key: Key('searchBar'),
-              decoration: InputDecoration(labelText: 'Search Shops'),
-              onChanged: (txt) {
-                //todo search here
-              },
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.search),
+                title: TextField(
+                  controller: _textEditingController,
+                  decoration: InputDecoration(
+                      hintText: 'Search Shops', border: InputBorder.none),
+                  onChanged: onSearchTextChanged,
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    _textEditingController.clear();
+                    onSearchTextChanged('');
+                  },
+                ),
+              ),
             ),
           ),
           Container(
             height: 30,
+            margin: EdgeInsets.only(bottom: 10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -52,20 +78,70 @@ class RepairShopList extends StatelessWidget {
           ),
           Container(
             height: MediaQuery.of(context).size.height * 0.8,
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return RepairShopCard();
-              },
-            ),
+            child: _buildListOfShops(),
           ),
         ],
       ),
     );
   }
 
-  Future<String> loadAsset(BuildContext context) async {
-    rootBundle.loadString('mock_data/repair_shops_mockJSON.json');
+  Widget _buildListOfShops() {
+    if (_textEditingController.text.isNotEmpty &&
+        repairShopsSearchResults.isEmpty) {
+      return Container(
+        child: Text('No repair shops that match your search term found.'),
+      );
+    }
+
+    if (repairShopsSearchResults.isNotEmpty) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        shrinkWrap: true,
+        itemCount: repairShopsSearchResults.length,
+        itemBuilder: (context, index) {
+          return RepairShopCard(repairShopsSearchResults[index]);
+        },
+      );
+    } else {
+      return ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        shrinkWrap: true,
+        itemCount: repairShops.length,
+        itemBuilder: (context, index) {
+          return RepairShopCard(repairShops[index]);
+        },
+      );
+    }
+  }
+
+  Future<List<RepairShopDTO>> loadAsset(BuildContext context) async {
+    String data = await rootBundle
+        .loadString('assets/mock_data/repair_shops_mockJSON.json');
+    var jsonResult = json.decode(data);
+    setState(() {
+      mapRepairShopsFromJson(jsonResult);
+    });
+  }
+
+  List<RepairShopDTO> mapRepairShopsFromJson(List<dynamic> response) {
+    response.forEach((item) {
+      repairShops.add(RepairShopDTO.fromJson(item));
+    });
+    return repairShops;
+  }
+
+  onSearchTextChanged(String txt) {
+    repairShopsSearchResults.clear();
+    if (txt.isEmpty) {
+      setState(() {});
+      return;
+    }
+
+    repairShops.forEach((item) {
+      if (item.shopName.toLowerCase().contains(txt.toLowerCase()))
+        repairShopsSearchResults.add(item);
+    });
+
+    setState(() {});
   }
 }
